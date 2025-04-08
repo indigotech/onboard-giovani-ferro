596b43c72efd94499e56b5ceebd8c327f9bc18d5
GFerro <giovani.ferro@taqtile.com.br>
2025-04-08 17:49:38 -0300

    build: change repository to use typescript

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import Fastify, { FastifyInstance } from "fastify";
+
+const fastify: FastifyInstance = Fastify({ logger: true });
+
+const PORT: number = 30001;
+
+fastify.get("/hello", async (): Promise<string> => {
+  return "Hello, World!";
+});
+
+const start = async (): Promise<void> => {
+  try {
+    await fastify.listen({ port: PORT });
+    fastify.log.info(`Server listening on ${PORT}`);
+  } catch (err: unknown) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
