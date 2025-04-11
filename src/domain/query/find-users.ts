b66684f2298a1edb2526fd621e9ca54815123134
GBFerro <giovaniferro29@gmail.com>
2025-04-11 14:57:12 -0300

    feat: add hash to password

---
+++ src/domain/query/find-users.ts
@@ -0,0 +1,15 @@
+import { UserResponse } from "../../models/response/user/user-response.types";
+import { getUsers } from "../../repository/db-repository";
+
+export async function findUsers(): Promise<UserResponse[]> {
+  const users = await getUsers()
+
+  const userResponse: UserResponse[] = users.map(user => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    birthDate: user.birthDate,
+  }))
+
+  return userResponse;
+}
